154eefd68efec88518cbe0049d7f83ad3478037e
Monty Preston <monty.preston5@gmail.com>
2020-07-23 21:11:27 -0700

    Formatting edits to urls and views. Created skeleton templates for adopt_app.

--- adopt_app/urls.py
+++ adopt_app/urls.py
@@ -3,7 +3,7 @@ from . import views as adopt_views
 
 
 urlpatterns = [
-    path('adopt', adopt_views.index, name="home"),
+    path('', adopt_views.index, name="home"),
     path('search', adopt_views.search, name="search"),
     path('search/query', adopt_views.search_query, name="query"),
     path('pet_detail/<int:id>', adopt_views.pet_detail, name="pet_detail"),
