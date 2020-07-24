c4b27ac8dbd164f0f195b089268eb09e6ff65e7f
MNeipp <ReamusLQ@gmail.com>
2020-07-23 19:31:00 -0600

    users_app, adoption_app, settings.py ready

---
+++ user_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views as user_views
+
+urlpatterns=[
+    path('login', user_views.login, name="login"),
+    path('login/new', user_views.register, name="register"),
+    path('logout', user_views.logout, name="logout"),
+    path('profile', user_views.user_profile, name="user_profile"),
+    path('profile/update', user_views.update_profile, name="update_user_profile"),
+    path('profile/update_password', user_views.update_password, name="update_password"),
+]
