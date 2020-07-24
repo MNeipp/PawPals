a88442b4d8162421c7d32b7dacf9e5b8a4c8a2d9
Monty Preston <monty.preston5@gmail.com>
2020-07-24 16:18:24 -0700

    Created Pet model to house API ids. Updated m2m field in Users model.

--- adopt_app/models.py
+++ adopt_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here
+
+
+class Pet(models.Model):
+    petfinder_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
--- user_app/models.py
+++ user_app/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from __future__ import unicode_literals
 from django.db import models
+from adopt_app.models import Pet
 import bcrypt, re
 # from django_mysql.models import ListCharField
 
@@ -55,7 +56,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    # has_faves = models.ListCharField(base_field=IntegerField, blank=True)
+    has_faves = models.ManyToManyField(Pet, related_name="faved_by")
     created_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
     objects = userManager()
