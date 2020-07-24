bf399d07dd795296a46eed9745a4a86a8ab8897f
Monty Preston <monty.preston5@gmail.com>
2020-07-23 20:57:26 -0700

    Updated typo in 'shelters/' views spelling.

--- adopt_app/urls.py
+++ adopt_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('search', adopt_views.search, name="search"),
     path('search/query', adopt_views.search_query, name="query"),
     path('pet_detail/<int:id>', adopt_views.pet_detail, name="pet_detail"),
-    path('shelters', adopt_views.shelter, name="shelters"),
+    path('shelters', adopt_views.shelters, name="shelters"),
     path('shetlers/<int:id>', adopt_views.shelter_detail, name="shelter_detail"),
 ]
