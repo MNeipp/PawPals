407d356b1ea8431cc1c25a9ab00dedd3c3fb3953
MNeipp <ReamusLQ@gmail.com>
2020-07-27 19:58:49 -0600

    search queries, shelters queries

--- adopt_app/urls.py
+++ adopt_app/urls.py
@@ -5,7 +5,6 @@ from . import views as adopt_views
 urlpatterns = [
     path('', adopt_views.index, name="home"),
     path('search/', adopt_views.search, name="search"),
-    path('search/query', adopt_views.search_query, name="query"),
     path('pets/<int:dog_id>', adopt_views.pet_detail, name="pet_detail"),
     path('shelters', adopt_views.shelters, name="shelters"),
     path('shelters/<str:shelter_id>', adopt_views.shelter_detail, name="shelter_detail"),
